34eaa08fd8e2549c686d32a026cea6235ba416b3
aguhernandez <aguhernandezbk@gmail.com>
2026-04-27 13:23:55 +0300

    Fix: crear carpetas temporales para engañar a Xcode

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+set -e
 
-# 1. Instalar CocoaPods
+# 1. Creamos carpetas vacías para que Xcode no se queje al principio
+mkdir -p node_modules/@capacitor/camera
+mkdir -p node_modules/@capacitor/geolocation
+mkdir -p node_modules/@capacitor/push-notifications
+
+# 2. Instalamos lo que realmente hace falta
 brew install cocoapods
-
-# 2. Ir a la raíz del proyecto para instalar dependencias de Node
-cd ../../
 npm install
-
-# 3. Sincronizar Capacitor para que genere el .xcworkspace
 npx cap sync ios
-
-# 4. Volver a la carpeta de scripts para que Xcode Cloud continúe
-cd ios/App/ci_scripts
