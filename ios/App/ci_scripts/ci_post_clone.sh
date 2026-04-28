a9103b549ceb960d3b0251133c20fd191603b416
aguhernandez <aguhernandezbk@gmail.com>
2026-04-28 10:26:50 +0300

    Fix: shebang corregido a /bin/zsh y permisos x

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,8 @@
-#!/zsh
+#!/bin/zsh
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/bin:$PATH"
-echo "🚀 INICIANDO CI_POST_CLONE"
+echo "🚀 SCRIPT INICIADO CORRECTAMENTE"
 cd ../../..
 npm install
 npx cap sync ios
 cd ios/App && pod install
-echo "✅ SCRIPT FINALIZADO"
-# Nuevo inicio
+echo "✅ TODO INSTALADO"
